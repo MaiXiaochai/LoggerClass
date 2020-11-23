56a1ef0c5326aa1fe22c890ee2d6ae65d66178b0
MaiXiaochai <maixiaochai@qq.com>
2020-11-23 15:35:55 +0800

    +)example.py *)done.

---
+++ example.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+--------------------------------------
+@File       : example.py
+@Author     : maixiaochai
+@Email      : maixiaochai@outlook.com
+@CreatedOn  : 2020/11/23 15:26
+--------------------------------------
+"""
+
+from logger import Logger
+
+
+def demo():
+    from time import sleep
+
+    log = Logger().log
+    print()
+    for i in range(10):
+        sleep(0.5)
+        log.info(i)
+
+
+if __name__ == '__main__':
+    demo()
